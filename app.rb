9691b0dea1d15c209c56bda05c9d10f104dc35e4
Oscar Gonzalez <ogonzal87@gmail.com>
2015-01-30 08:50:54 -0500

    Search ability is now working

--- app.rb
+++ app.rb
@@ -6,12 +6,20 @@ require_relative './model/recipes'
 get '/' do
 	erb :home
 end
+
 	
-get '/drinks_all' do 
-	erb :search_all
+get '/drinksall' do
+  @search = params[:search]
+  if @search && @search.length > 0
+    @drinksall = Recipes.search(@search)
+  else
+    @drinksall = Recipes.all
+  end
+  erb :drinksall
 end
 
-get '/drink' do
+get '/drinksall/:id' do
+	@drink = Recipes.find(params[:id].to_i)
 	erb :drink
 end
 
--- model/recipes.rb
+++ model/recipes.rb
@@ -7,33 +7,32 @@ class Recipes
       Recipe.new(id: 1, 
         name: 'Gin Gimlet', 
         description: 'jjh', 
-        ingredients: '1.25 oz Tanqueray gin, 1 oz Rose-s lime juice, 1 twist lime', 
+        ingredients: 'test', 
         time: 'djh', 
-        directions: 'Pour the gin and lime juice into a mixing glass half-filled with ice cubes. Stir well. Strain into a cocktail glass and garnish with the lime wedge.'),
+        directions: 'test'),
       Recipe.new(id: 2, 
         name: 'Side Car', 
         description: 'kjh', 
-        ingredients: '3/4 oz triple sec, 1/2 oz cognac, 3/4 oz lemon juice', 
+        ingredients: 'test', 
         time: 'Less than 5 minutes!', 
-        directions: 'Shake ingredients with ice and strain into a chilled cocktail glass. Garnish with a twist of lemon peel.'),
+        directions: ''),
       Recipe.new(id: 3, 
         name: 'Old Fashioned', 
         description: 'mkjhg', 
-        ingredients: '2 oz bourbon whiskey, 2 dashes Angostura® bitters, 1 splash water, 1 tsp sugar, 1 maraschino cherry, 1 orange wedge', 
+        ingredients: '', 
         time: 'kh', 
-        directions: 'Mix sugar, water and angostura bitters in an old-fashioned glass. Drop in a cherry and an orange wedge. Muddle into a paste using a muddler or the back end of a spoon. Pour in bourbon, fill with ice cubes, and stir.')
+        directions: 'test')
     ]
   end
 
   def self.find(id)
-    all.find do |item|
+    self.all.find do |item|
       item.id == id
     end
   end
 
-  def self.search(pattern)
+ def self.search(pattern)
     all.select do |item|
-      item.category.downcase.include?(pattern.downcase) ||
       item.name.downcase.include?(pattern.downcase) ||
       item.description.downcase.include?(pattern.downcase)
     end
